10094533ab22a82cfba6668c236acf6e90fe7f0b
Ivan Barcia <ivan.barcia@gmail.com>
2021-08-10 14:10:42 -0300

    Correct Telegram token

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 PORT = int(os.environ.get('PORT', 8443))
-TELEGRAM_TOKEN = '1853524252:AAExFi4N1SqempiOew6sQ5TQEIxIHqcMQo4'
+TELEGRAM_TOKEN = '1912436438:AAHAg1mIzoCA4bfK4zUHnewkCEB3kGO8cDU'
 HEROKU_URL = "https://tranquil-tor-03771.herokuapp.com/"
